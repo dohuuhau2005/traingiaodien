e5e8827e22932a1a0e8d9e40555d24fea71c858d
dohuuhau <dhhsvip@gmail.com>
2025-03-09 16:40:36 +0700

    train

---
+++ app/src/main/java/com/nhom79/train123/MainActivity.java
@@ -0,0 +1,36 @@
+package com.nhom79.train123;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+ImageView imgMenu,imgUserName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        imgMenu=findViewById(R.id.imgMenu);
+        imgUserName=findViewById(R.id.imgUserName);
+        imgUserName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,User_activity.class));
+                finish();
+            }
+        });
+    }
+}
